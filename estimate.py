f6d9d08e9f98aa93805c6b11107926e41e3288be
Rijoy John <johnrijoy@gmail.com>
2021-05-29 21:25:13 +0530

    First attempt in coding solution

--- estimate.py
+++ estimate.py
@@ -1,5 +1,6 @@
 import math
 import unittest
+import random
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -27,6 +28,43 @@ class TestMC(unittest.TestCase):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
+# Solution Code
+def wallis(n):
+    '''
+    input:
+    n - number of iterations
+    output:
+    estpi - numerical estimate of pi using wallis formula
+    '''
+    estpi = 2 # variable used to store the value obtained from wallis formula iteratively
+    for i in range(1, n+1):
+        estpi *= (4*(i**2))/((4*(i**2))-1)
+    
+    return estpi
+
+def monte_carlo(n):
+    '''
+    side of square is 1 unit
+    Area of circle estimated by checking the the number of darts falling within the circular  region
+    input:
+    n - number of simulations
+    output:
+    estpi - numerical estimate of pi using monte carlo formula
+    '''
+    
+    dsq = float(n) # darts in square region represent the square`s area
+    dcr = 0.00 # darts in circular region represnting circles`s area
+    
+    for i in range(n):  # running monte carlo simulations
+        x, y =  random.random(), random.random() # generating random points in plane
+        
+        r = ((x-0.5)**2 + (y-0.5)**2)**0.5 # finding distance of the point from circle`s centre
+        
+        if r <= 0.5:  # checking whether the point lies within the circle
+            dcr += 1  
+     
+    estpi = 4*dcr/dsq
+    return estpi 
     
 if __name__ == "__main__":
     unittest.main()
